c2fd917475c9ebf9cf9d5b436b4ee1afac3ffa6e
Daniel Oscar Zamo <daniel.oscar@gmail.com>
2023-05-16 00:21:18 +0200

    add index.html, .gitignore, cli-codes

---
+++ cli-codes/_1.initial_fedora38-minimal.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+# Function to display the confirmation prompt
+function confirm() {
+    while true; do
+        read -p "Ejecutar la configuración mínima/inicial de Fedora 38? (YES/NO/CANCEL) " yn
+        case $yn in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            [Cc]* ) exit;;
+            * ) echo "Ingresar YES, NO, or CANCEL.";;
+        esac
+    done
+}
+
+# Example usage of the confirm function
+if confirm; then
+    echo "User chose YES. Executing the operation..."
+    ### Configurar usuario para `sudo`
+    echo "$USER ALL=(ALL) NOPASSWD:ALL"|sudo tee /etc/sudoers.d/$USER
+    ### Configurar usuario para usuario su (admin en wheel)
+    sudo usermod -G wheel -a $USER
+    sudo sed -i \
+      's/^#auth\t\trequired\tpam_wheel.so use_uid$/&\nauth\t\trequired\tpam_wheel.so use_uid/' \
+      /etc/pam.d/su
+    ### Deshabilitar _firewalld_ y _SELinux_
+    sudo systemctl disable --now firewalld
+    sudo sed -i.org 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+    ### Configuración Dnf (flags)
+    echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
+    echo 'max_parallel_downloads=6' | sudo tee -a /etc/dnf/dnf.conf
+    echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
+    ### Actualizar paquetes del sistema
+    sudo dnf upgrade -y --refresh
+    sudo dnf -y update
+    # Agregar repo y habilitar fedora-third-party
+    sudo dnf -y install fedora-workstation-repositories
+    ## sudo dnf install fedora-repos fedora-repos-modular
+    # Repositorios Fusion
+    sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm
+    sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
+    dnf install -y rpmfusion-free-appstream-data rpmfusion-nonfree-appstream-data
+    
+    # Agregar mas repositorios
+    sudo dnf install createrepo_c
+    sudo dnf upgrade -y --refresh
+    
+    ### Instalación paquetes (para CLI)
+    sudo dnf -y install \
+      git htop vim \
+      rsync openssh-server openssh-clients
+    
+    ### Habilitar/instalar flatpak
+    sudo dnf -y install flatpak
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    flatpak update
+    ### Deshabilitar swap sobre zram
+    sudo systemctl disable --now systemd-zram-setup@zram0.service
+    sudo dnf remove -y zram-generator zram-generator-defaults
+    sudo swapoff -a && sudo swapon -a
+    echo "Se han ejecutado las tareas."
+else
+    echo "No se ejecuto nada. Saliendo del script..."
+fi
+
