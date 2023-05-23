ac4d5f4bc1d93cf8a9206851608b8c5ac8757901
Daniel Oscar Zamo <daniel.oscar@gmail.com>
2023-05-23 14:52:16 +0200

    rebuilding site Tue May 23 02:52:15 PM CEST 2023

--- cli-codes/apps.desktop.install.sh
+++ cli-codes/apps.desktop.install.sh
@@ -3,3 +3,4 @@ sudo dnf -y install openshot
 sudo dnf -y install https://cdn.insynchq.com/builds/linux/insync-3.8.6.50504-fc38.x86_64.rpm
 # El PATH exacto de la versión a instalar puede variar. Se recpmienda revisar en el URL oficial de la aplicación.
 sudo dnf -y install https://cdn.insynchq.com/builds/linux/insync-dolphin-3.7.9.50368-1.noarch.rpm
+sudo dnf -y install kate kate-plugins perl-Syntax-Highlight-Engine-Kate
