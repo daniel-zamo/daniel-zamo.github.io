1aea053f76d689b276cee19e79dec715643b8da0
Daniel Oscar Zamo <daniel.oscar@gmail.com>
2023-05-23 13:45:24 +0200

    first script

---
+++ cli-codes/apps.desktop.install.sh
@@ -0,0 +1,5 @@
+sudo dnf -y install openshot
+# El PATH exacto de la versión a instalar puede variar. Se recpmienda revisar en el URL oficial de la aplicación.
+sudo dnf -y install https://cdn.insynchq.com/builds/linux/insync-3.8.6.50504-fc38.x86_64.rpm
+# El PATH exacto de la versión a instalar puede variar. Se recpmienda revisar en el URL oficial de la aplicación.
+sudo dnf -y install https://cdn.insynchq.com/builds/linux/insync-dolphin-3.7.9.50368-1.noarch.rpm
