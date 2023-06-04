e72e11c0ed85ebdcd1e86ab036d8b20dfb466488
Daniel Oscar Zamo <daniel.oscar@gmail.com>
2023-06-04 13:35:39 +0200

    rebuilding site Sun Jun  4 01:35:39 PM CEST 2023

--- cli-codes/apps.developer.sh
+++ cli-codes/apps.developer.sh
@@ -3,15 +3,15 @@ sudo dnf -y install kate kate-plugins perl-Syntax-Highlight-Engine-Kate
 # Instalación de hugo
 sudo dnf -y install hugo
 # Instalación de Visual Code Studio
-#cat <<EOF |sudo tee /etc/yum.repos.d/vscode.repo
-#[vscode]
-#name=Visual Studio Code
-#baseurl=https://packages.microsoft.com/yumrepos/vscode
-#enabled=1
-#gpgcheck=1
-#priority=20
-#gpgkey=https://packages.microsoft.com/keys/microsoft.asc
-#EOF
-#sudo dnf -y update --refresh
-#sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc 
-#sudo dnf -y install code
+cat <<EOF |sudo tee /etc/yum.repos.d/vscode.repo
+[vscode]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+priority=20
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+sudo dnf -y update --refresh
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc 
+sudo dnf -y install code
