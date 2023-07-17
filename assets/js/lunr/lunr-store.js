a5dcbf0ca41740bf26a5f69d0a0467fcc303027f
Daniel Oscar Zamo <daniel.oscar@gmail.com>
2023-07-18 00:31:12 +0200

    site first

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "writing-jekyll-posts",
+        "excerpt":"hola mundo!      Referencias de pagina a usar. Esta basada en el Theme y tiene post de K8s … Usa el theme.            Sitio de Christian Posta.           ","categories": [],
+        "tags": [],
+        "url": "/writing-jekyll-posts/",
+        "teaser": null
+      }]
