4c88ba70ba3c692c4366514056d6423bae5fe602
Daniel Oscar Zamo <daniel.oscar@gmail.com>
2023-08-26 00:25:15 +0200

    Sat Aug 26 12:25:15 AM CEST 2023

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230826.002412';
+const cacheName = 'chirpy-20230826.002515';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
